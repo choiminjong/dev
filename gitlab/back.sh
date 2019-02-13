de6ca2cad990849222c0dfeb2e058e62311d0144
dominic <37336360+choiminjong@users.noreply.github.com>
2019-02-13 10:21:41 +0900

    Update back.sh

--- gitlab/back.sh
+++ gitlab/back.sh
@@ -14,34 +14,29 @@ DEFAULT_DIR="/opt/atlassian/jira/conf"
 #GIT FOLDER
 GIT_DIR="/home/jira-conf/jira"
 
-echo "CI_COMMIT_TITLE=>"$CI_COMMIT_TITLE
-echo "CI_COMMIT_MESSAGE=>"$CI_COMMIT_MESSAGE
+#echo "CI_COMMIT_TITLE=>"$CI_COMMIT_TITLE
+#echo "CI_COMMIT_MESSAGE=>"$CI_COMMIT_MESSAGE
 
 if [ $SRC_DIR ] #파일 명 인자 체크
 then
-	if [ -d $TEMP_DIR ] #파일 유무 체크
+	if [ -f $DEFAULT_DIR/$CI_COMMIT_TITLE ] #파일 유무 체크
 	then
-		echo "파일이 존재합니다."
-		
-		if [ -f $DEFAULT_DIR/$CI_COMMIT_TITLE ] #파일 유무 체크
+		if [ -d $TEMP_DIR ] #파일 유무 체크
 		then
-			echo "존재하는 파일 이동"
+			echo "moving files"
 			\cp $DEFAULT_DIR/$CI_COMMIT_TITLE $TEMP_DIR/$CI_COMMIT_TITLE.`date +"%Y-%m-%d"`-`date +"%H:%M:%S"`
 			\cp $GIT_DIR/$CI_COMMIT_TITLE $DEFAULT_DIR/$CI_COMMIT_TITLE
-		fi
-		
-	elif [ ! -d $TEMP_DIR ] 
-	then
-		echo "파일이 존재안합니다."
-		
-		if [ -f $DEFAULT_DIR/$CI_COMMIT_TITLE ] #파일 유무 체크
+		elif [ ! -d $TEMP_DIR ] 
 		then
-			echo "존재하는 파일 이동"
+			echo "moving files after folder creation"
 			mkdir $TEMP_DIR
 			chown $USER:$USER $TEMP_DIR			
 			\cp $DEFAULT_DIR/$CI_COMMIT_TITLE $TEMP_DIR/$CI_COMMIT_TITLE.`date +"%Y-%m-%d"`-`date +"%H:%M:%S"`
 			\cp $GIT_DIR/$CI_COMMIT_TITLE $DEFAULT_DIR/$CI_COMMIT_TITLE
-		fi
+		fi	
+	else
+		echo "Not exists. "$CI_COMMIT_TITLE
+		exit 1
 	fi	
 fi
 
